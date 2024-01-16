2ee999b6d1ca308d1509591b95114a591253b400
VaddymK12 <komisvadym@gmail.com>
2024-01-17 01:54:28 +0200

    CorrectionCoordinats commit

--- client/src/EnemyMap/EnemyMap.jsx
+++ client/src/EnemyMap/EnemyMap.jsx
@@ -2,13 +2,16 @@ import React, { useState } from 'react';
 import Cell from '../Cell/Cell';
 
 const EnemyMap = () => {
-    const cells = Array.from({length: 100}, (_, i) => <Cell key={i} isEnemy={true} x={String(i+1)[1]} y={String(i+1)[0]} />);
+  const cells = Array.from({ length: 100 }, (_, i) => (
+    <Cell key={i} isEnemy={true} x={(i % 10) + 1} y={Math.floor(i / 10) + 1} />
+  ));
 
-    return (
-        <div style={{display: "grid", gridTemplateColumns: "repeat(10, 30px)", gridAutoRows: "30px", margin: 50}}>
-            {cells.map(cell => cell)}
-        </div>
-    );
+  return (
+    <div style={{ display: "grid", gridTemplateColumns: "repeat(10, 30px)", gridAutoRows: "30px", margin: 50 }}>
+      {cells.map(cell => cell)}
+    </div>
+  );
 }
 
 export default EnemyMap;
+
--- client/src/MyMap/MyMap.jsx
+++ client/src/MyMap/MyMap.jsx
@@ -1,16 +1,17 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Cell from '../Cell/Cell';
 
 const MyMap = () => {
-    const cells = Array.from({ length: 100 }, (_, i) => {
-        return <Cell key={i} x={Number(String(i+1)[1])} y={Number(String(i+1)[0])}/>;
-    });
-    
-    return (
-        <div style={{display: "grid", gridTemplateColumns: "repeat(10, 30px)", gridAutoRows: "30px", margin: 50}}>
-            {cells.map(cell => cell)}
-        </div>
-    );
+  const cells = Array.from({ length: 100 }, (_, i) => (
+    <Cell key={i} x={(i % 10) + 1} y={Math.floor(i / 10) + 1} />
+  ));
+
+  return (
+    <div style={{ display: "grid", gridTemplateColumns: "repeat(10, 30px)", gridAutoRows: "30px", margin: 50 }}>
+      {cells.map(cell => cell)}
+    </div>
+  );
 }
 
 export default MyMap;
+
