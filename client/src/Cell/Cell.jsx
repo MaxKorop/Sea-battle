aa3f832cba0493e96c1ff6af09f0a6add178941f
VaddymK12 <komisvadym@gmail.com>
2024-01-16 19:40:35 +0200

    CellChanges

--- client/src/Cell/Cell.jsx
+++ client/src/Cell/Cell.jsx
@@ -1,31 +1,43 @@
 import React, { useState } from 'react';
 
 const Cell = ({ isEnemy, x, y, isShip }) => {
+  const [showMiss, setShowMiss] = useState(false);
+  const [hasClicked, setHasClicked] = useState(false);
 
-    const onClickCell = () => {
-        setStyle({...style, backgroundColor: 'gray'});
+  const onClickCell = () => {
+    if (!hasClicked && isEnemy) {
+      setShowMiss(true);
+      setHasClicked(true);
+      console.log(x, y);
     }
+  };
 
-
-    const [style, setStyle] = useState({
+  return (
+    <div
+      style={{
         width: 30,
         height: 30,
-        border: "1px dashed black"
-    });
-    
-    if (isEnemy) {
-        return (
-            <div style={style} onClick={onClickCell}>
-                
-            </div>
-        );
-    } else {
-        return (
-            <div style={style}>
-                
-            </div>
-        );
-    }
-}
+        border: "1px dashed black",
+        position: 'relative',
+      }}
+      onClick={isEnemy ? onClickCell : undefined}
+    >
+      {/* Додайте надпис "miss", якщо showMiss === true */}
+      {isEnemy && showMiss && (
+        <div
+          style={{
+            position: 'absolute',
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
+            color: 'red', // колір тексту
+          }}
+        >
+          miss
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default Cell;
