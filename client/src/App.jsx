4a05826d3393d8c9fc6c033af52518752d09ddda
VaddymK12 <komisvadym@gmail.com>
2024-01-23 21:56:07 +0200

    StartGamePage commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,17 @@
 import React from 'react';
-import MyMap from './Map/MyMap';
-import EnemyMap from './Map/EnemyMap';
-import RoomCode from './RoomCode/RoomCode';
-import Title from './Title/Title';
-import StartButton from './StartButton/StartButton';
+import GamePage from './GamePage';
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import StartPage from './StartPage';
 
 const App = () => {
   return (
     <div>
-      <RoomCode />
-      <Title />
-      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center'}}>
-        <div style={{display: "flex", flexDirection: "row", justifyContent: "space-around", paddingTop: 35, width: '100%' }}>
-          <MyMap />
-          <EnemyMap />
-        </div>
-        <StartButton />
-      </div>
+      <BrowserRouter>
+         <Routes>
+           <Route path='/' element={<StartPage/>}/>
+           <Route path="/game" element={<GamePage />} />
+         </Routes>
+      </BrowserRouter>
     </div>
   );
 }
